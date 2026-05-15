ce00cdaf8d232fee95ce669becbeba34762aa49c
Osama <o.ma7rous10@gmail.com>
2026-05-15 21:20:59 +0300

    done OrderItemService

--- src/main/java/com/example/eCommerce/service/OrderItemService.java
+++ src/main/java/com/example/eCommerce/service/OrderItemService.java
@@ -1,5 +1,6 @@
 package com.example.eCommerce.service;
 
+import com.example.eCommerce.Mapper.OrderMapper;
 import com.example.eCommerce.dto.orderItemDto.OrderItemResponseDTO;
 import com.example.eCommerce.entity.OrderItem;
 import com.example.eCommerce.repository.OrderItemRepo;
@@ -10,33 +11,39 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class OrderItemItemService {
+public class OrderItemService {
     private final OrderItemRepo orderItemRepo;
+    private final OrderMapper orderMapper;
 
 
     public OrderItemResponseDTO createOrderItem(OrderItem item) {
-
-
+        OrderItem save = orderItemRepo.save(item);
+        return orderMapper.orderItemToDto(save);
     }
 
-    public OrderItemResponseDTO updateOrderItem(Long id, OrderItem
-            order) {
-
+    public OrderItemResponseDTO updateOrderItem(Long id, OrderItem order) {
+        OrderItem oldOrderItem = orderItemRepo.findById(id).orElseThrow(() -> new RuntimeException("OrderItem not found"));
+        oldOrderItem.setQuantity(order.getQuantity());
+        oldOrderItem.setPrice(order.getPrice());
+        oldOrderItem.setProduct(order.getProduct());
+        orderItemRepo.save(oldOrderItem);
+        return orderMapper.orderItemToDto(oldOrderItem);
     }
 
     public String deleteOrderItem(Long id) {
-
+        orderItemRepo.deleteById(id);
         return "OrderItem Deleted successfully";
 
     }
 
     public OrderItemResponseDTO getOneOrderItem(Long id) {
-
-
+        OrderItem orderItem = orderItemRepo.findById(id).orElseThrow(() -> new RuntimeException("OrderItem not found"));
+        return orderMapper.orderItemToDto(orderItem);
     }
 
     public List<OrderItemResponseDTO> getOrderItems() {
-
+        List<OrderItem> all = orderItemRepo.findAll();
+        return orderMapper.orderItemToDtoList(all);
     }
 
 }
